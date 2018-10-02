df24577b6b094dda906c69cd53ec117555d39427
Nicolas <pereznicolas90@gmail.com>
2018-10-02 18:31:49 -0500

    Ahora si
    jeje

---
+++ app/src/main/java/com/example/nicolasperez/mytutoreia/RegistroGeneral.java
@@ -0,0 +1,39 @@
+package com.example.nicolasperez.mytutoreia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RegistroGeneral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_general);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void registroEstudiante(View view){
+        Intent intent = new Intent(this, RegistroEstudiante.class);
+        startActivity(intent);
+    }
+
+    public void registroAsesor(View view){
+        Intent intent = new Intent(this, RegistroAsesor.class);
+        startActivity(intent);
+    }
+}
